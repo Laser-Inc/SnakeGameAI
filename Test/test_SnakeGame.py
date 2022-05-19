246b0b18ed3fb61d46d88d5c506acbc0d310e29a
jmorr333 <jmorr333@ford.com>
2022-05-19 14:59:36 +0100

    Added test to check if snake was drawn + extracted the draw_snake method

--- Test/test_SnakeGame.py
+++ Test/test_SnakeGame.py
@@ -1,7 +1,8 @@
 import unittest
+import pygame
 
 from SnakeGameAI.src.Snake import Snake
-from SnakeGameAI.src.SnakeGame import WIDTH, HEIGHT
+from SnakeGameAI.src.SnakeGame import WIDTH, HEIGHT, screen, draw_snake
 
 
 class SnakeGameTest(unittest.TestCase):
@@ -9,5 +10,9 @@ class SnakeGameTest(unittest.TestCase):
         snake = Snake(((WIDTH / 2), (HEIGHT / 2)))
         self.assertEqual(((WIDTH / 2), (HEIGHT / 2)), snake.get_initial_position())
 
-    #def test_check_if_snake_drawn(self):
-
+    def test_check_if_snake_drawn(self):
+        snake = Snake((int((WIDTH / 2)), int((HEIGHT / 2))))
+        GREEN = (0, 255, 0)
+        draw_snake()
+        print(screen.get_at(snake.get_initial_position()))
+        self.assertEqual(GREEN, screen.get_at(snake.get_initial_position()))
--- src/SnakeGame.py
+++ src/SnakeGame.py
@@ -2,20 +2,34 @@ import sys
 
 import pygame
 
+from SnakeGameAI.src.Snake import Snake
+
 pygame.init()
 
 WIDTH = 500
 HEIGHT = 500
+CELL_WIDTH = 10
+CELL_HEIGHT = 10
+GREEN = (0, 255, 0)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+snake = Snake(((WIDTH / 2), (HEIGHT / 2)))
 
 
 def main():
+    draw_snake()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+
+def draw_snake():
+    pygame.draw.rect(screen, GREEN,
+                     pygame.Rect(snake.get_initial_position()[0], snake.get_initial_position()[1], CELL_WIDTH,
+                                 CELL_HEIGHT))
+    pygame.display.flip()
 
 
 if __name__ == '__main__':
